0e16d9dc84de88bd1287cccded5efd377634755e
narmada1004 <narmada.karnati@gmail.com>
2019-12-19 04:42:28 -0600

    second commit

--- src/test/java/mymaven/RestapiTest.java
+++ src/test/java/mymaven/RestapiTest.java
@@ -7,12 +7,15 @@ public class RestapiTest {
 	public void api()
 	{
 		System.out.println("api method");
+		System.out.println("api method 2");
 	}
 	@Test
 	public void deletetwitter()
 	{
 	System.out.println("deletetwitter");
+	System.out.println("deletetwitter 2");
 	}
+
 	
 
 }
